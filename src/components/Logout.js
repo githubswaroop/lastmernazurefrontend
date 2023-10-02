39ac5a30d2f194485219588e6522cf3771bd085b
githubswaroop <96583547+githubswaroop@users.noreply.github.com>
2023-10-02 20:49:54 +0530

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -4,33 +4,35 @@ import { Usercontext } from '../App';
 
 
 function Logout() {
+  localStorage.removeItem("JWT");
+  navigate("/login");
   // const {state,dispatch}=useContext(Usercontext);
-const navigate=useNavigate();
-   useEffect(()=>{
-    fetch("/logout",{
-        method:"GET",
-        headers:{
-          Accept:"application/json",
-          "Content-Type":"application/json"
-        },
-        credentials:'include'
-      }).then((res)=>{
-        //  dispatch({type:"USER",payload:false});
-         navigate('../',{replace:true});
-         if(res.status!==200){
-          console.log("error frommmmmmmmmmmmm logout")
-           const error=new Error(res.error);
-           throw error;
-         }
+// const navigate=useNavigate();
+//    useEffect(()=>{
+//     fetch("/logout",{
+//         method:"GET",
+//         headers:{
+//           Accept:"application/json",
+//           "Content-Type":"application/json"
+//         },
+//         credentials:'include'
+//       }).then((res)=>{
+//         //  dispatch({type:"USER",payload:false});
+//          navigate('../',{replace:true});
+//          if(res.status!==200){
+//           console.log("error frommmmmmmmmmmmm logout")
+//            const error=new Error(res.error);
+//            throw error;
+//          }
           
             
          
 
-      }).catch((err)=>{
+//       }).catch((err)=>{
         
-           console.log(err);
-      })
-   })
+//            console.log(err);
+//       })
+//    })
   return (
    
     <div>
