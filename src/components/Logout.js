a07994623fd39e6831015a337d4e177c7204c4a9
githubswaroop <96583547+githubswaroop@users.noreply.github.com>
2023-10-02 21:17:27 +0530

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -7,7 +7,7 @@ function Logout() {
   const navigate=useNavigate();
     const location=useLocation();
   localStorage.removeItem("JWT");
-   navigate("../");
+   
   // const {state,dispatch}=useContext(Usercontext);
 // const navigate=useNavigate();
 //    useEffect(()=>{
