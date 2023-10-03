90829e702a5f21743e8567c7436053b26943903a
githubswaroop <96583547+githubswaroop@users.noreply.github.com>
2023-10-03 21:49:03 +0530

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,24 +1,21 @@
-import React,{useEffect,useContext} from 'react';
-import { useLocation, useNavigate,} from 'react-router-dom';
+import React, { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Usercontext } from '../App';
 
-
 function Logout() {
-  
-     
+  const navigate = useNavigate();
+
   useEffect(() => {
-    const navigate=useNavigate();
-    
- 
+    // Remove the 'JWT' variable from local storage
+    localStorage.removeItem('JWT');
 
-localStorage.removeItem('JWT');
-
-    window.location.href = './Login';
+    // Navigate to the home page and replace the current page in the history
+    navigate('../', { replace: true });
+  }, []); // Empty  array to ensure the effect runs once
 
   return (
-   
     <div>
-    
+     
     </div>
   );
 }
